3394c5f6dc4200602fce00e04d0db8e405161dd5
dyoshikawa <dyoshikawa1993@gmail.com>
2020-03-22 16:45:09 +0900

    init

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,29 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
+use tera::Tera;
 
-#[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+// store tera template in application state
+async fn index(
+    tmpl: web::Data<tera::Tera>,
+) -> Result<HttpResponse, Error> {
+    let s = tmpl
+        .render("index.html", &tera::Context::new())
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    std::env::set_var("RUST_LOG", "actix_web=info");
+
+    HttpServer::new(|| {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/template/**/*")).unwrap();
+
+        App::new()
+            .data(tera)
+            .wrap(middleware::Logger::default()) // enable logger
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
